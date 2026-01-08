9bc8954f5e325cf99fd0a2ed407fc459505a2764
dinhtuan1303 <40958746+dinhtuan1303@users.noreply.github.com>
2026-01-08 14:24:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ st.set_page_config(page_title="Hệ thống QNU", layout="wide")
 st.sidebar.title("DANH MỤC")
 
 # Thiết lập danh sách lựa chọn
-option_1 = "Giấy đề nghị thanh toán"
+option_1 = "Giấy đề nghị tạm ứng"
+option_2 = "Giấy đề nghị thanh toán"
 
-menu = st.sidebar.selectbox("Chọn loại mẫu:", [option_1])
+menu = st.sidebar.selectbox("Chọn loại mẫu:", [option_1, option_2])
 
 # Sửa lỗi 2: Đảm bảo điều kiện if khớp hoàn toàn với selectbox
 if menu == option_1:
     mauso3_tamung.hien_thi()
-elif menu == option_1:
-
+elif menu == option_2:
     mauso4_1_thanhtoan.hien_thi()
